78570958b51291fbc7b7556188ac16b5007b0df9
Sam Davis <sam@sjd.co>
2024-01-11 15:37:16 +1100

    Slight redesign and content pass

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
     title: 'Sam Davis - Web Developer',
-    description: 'A passionate web developer working out of Sydney, Australia',
+    description: 'A passionate web developer working in Sydney, Australia',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -16,8 +16,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 <meta name="theme-color" content="#1A0033" />
             </head>
             <body className={`${inter.className} bg-gradientPurpleDark`}>
-                <div className="bg-gradient-radial-sm fixed left-0 top-0 h-lvh w-screen"></div>
-                <div className="z-1 relative text-sm leading-relaxed text-white text-opacity-50 selection:bg-purple selection:text-gradientPurpleDark sm:text-base sm:leading-relaxed">
+                <div className="fixed left-0 top-0 h-lvh w-screen bg-gradient-radial-sm"></div>
+                <div className="z-1 relative text-pretty text-sm leading-relaxed text-white text-opacity-50 selection:bg-purple selection:text-gradientPurpleDark sm:text-base sm:leading-relaxed">
                     {children}
                 </div>
             </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Intro from '../content/intro.mdx';
-import { ContentBlock } from './components/ContentBlock';
 import employment from '../content/employment-history.json';
 import technicalSkills from '../content/technical-skills.json';
 import { SocialIcons } from './components/SocialIcons';
@@ -7,13 +6,13 @@ import { SocialIcons } from './components/SocialIcons';
 export default function Home() {
     return (
         <div className="container mx-auto flex min-h-screen max-w-screen-xl flex-col items-start gap-12 px-6 py-12 sm:px-16 sm:py-16 lg:flex-row lg:gap-16 lg:py-24">
-            <aside className="top-24 flex w-full flex-col gap-3 lg:sticky lg:max-w-xs">
+            <aside className="top-24 flex w-full flex-col gap-3 lg:sticky lg:max-w-[300px] xl:max-w-[390px]">
                 <h1 className="text-4xl font-bold text-purple sm:text-5xl">Sam Davis</h1>
                 <h5 className="text-lg text-white sm:text-xl">Web Developer</h5>
                 <p>
                     A passionate web developer
                     <br />
-                    working out of Sydney, Australia
+                    working in Sydney, Australia
                 </p>
                 <SocialIcons />
             </aside>
@@ -27,30 +26,48 @@ export default function Home() {
                     </h3>
                     <div className="mt-4 sm:mt-8">
                         {employment.jobs.map((job) => (
-                            <ContentBlock
-                                key={job.company}
-                                title={job.company}
-                                secondaryTitle={job.years}
-                                tertiaryTitle={job.title}
-                                content={
-                                    <ul className="list-disc pl-8">
-                                        {job.bulletPoints.map((point) => (
-                                            <li key={point.content} className="">
-                                                {point.content}
-                                                {point.children ? (
-                                                    <ul className="list-disc pl-8">
-                                                        {point.children.map((child) => (
-                                                            <li key={child.content}>
-                                                                {child.content}
-                                                            </li>
-                                                        ))}
-                                                    </ul>
-                                                ) : null}
-                                            </li>
+                            <div className="mt-8 first:mt-0 sm:flex" key={job.company}>
+                                <div className="hidden sm:mb-0 sm:block sm:w-[150px] sm:flex-shrink-0 sm:text-base sm:text-white md:w-[200px] md:text-lg">
+                                    {job.years}
+                                </div>
+                                <div className="flex w-auto flex-col">
+                                    <div className="flex-shrink-0 ">
+                                        <h4 className="mb-1 text-base text-white md:text-lg">
+                                            <span>{job.company}</span>{' '}
+                                            <span className="text-right text-white text-opacity-50 sm:hidden">
+                                                ({job.years})
+                                            </span>
+                                        </h4>
+                                    </div>
+                                    <div className="mt-1">
+                                        {job.titles.map((t, i) => (
+                                            <p
+                                                key={t}
+                                                className={`mt-2 text-xs font-semibold uppercase text-white text-opacity-70 first:mt-0 md:font-semibold ${
+                                                    i == 0 ? '' : 'text-opacity-30 line-through'
+                                                }`}
+                                            >
+                                                {t}
+                                            </p>
                                         ))}
-                                    </ul>
-                                }
-                            />
+                                    </div>
+                                    {job.content && (
+                                        <div className="mt-2 text-sm leading-relaxed">
+                                            {job.content}
+                                        </div>
+                                    )}
+                                    <div className="mt-4 inline-flex flex-wrap gap-2">
+                                        {job.technologies.map((t) => (
+                                            <span
+                                                className="rounded-md border-[1px] border-purple border-opacity-15 bg-purple bg-opacity-10 px-1.5 py-1 text-xs text-purple"
+                                                key={t}
+                                            >
+                                                {t}
+                                            </span>
+                                        ))}
+                                    </div>
+                                </div>
+                            </div>
                         ))}
                     </div>
                 </div>
@@ -58,7 +75,14 @@ export default function Home() {
                     <h3 className="text-xl font-bold text-purple sm:text-2xl">Technical skills</h3>
                     <div className="pt-4 sm:pt-4">
                         {technicalSkills.skills.map((skill, i) => (
-                            <ContentBlock key={i} title={skill.category} content={skill.content} />
+                            <div className="mt-8 flex w-auto flex-col first:mt-0 sm:flex" key={i}>
+                                <div className="flex-shrink-0 ">
+                                    <h4 className="mb-1 text-base text-white md:text-lg">
+                                        <span>{skill.category}</span>
+                                    </h4>
+                                </div>
+                                <div className="mt-1 text-sm leading-relaxed">{skill.content}</div>
+                            </div>
                         ))}
                     </div>
                 </div>

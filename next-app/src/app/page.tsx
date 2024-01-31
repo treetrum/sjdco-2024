a4f96a3991f24193da565d178ef394ad08c86dab
Sam Davis <sam@sjd.co>
2024-01-31 11:16:43 +1100

    Use env for cms url in next app

--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -3,7 +3,7 @@ import { HomePage } from './HomePage';
 
 export default async function Home() {
     const data: Home = await fetch(
-        `http://localhost:3000/api/globals/home?locale=undefined&draft=false&depth=1`,
+        `${process.env.NEXT_PUBLIC_CMS_URL}/api/globals/home?locale=undefined&draft=false&depth=1`,
     ).then((res) => res.json());
 
     return <HomePage data={data} />;

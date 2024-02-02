6271a6222a787e8fd858351e94148121085cd2b6
Sam Davis <sam@sjd.co>
2024-02-02 15:49:26 +1100

    Forced CI

--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -4,6 +4,7 @@ import { PayloadClient } from '@/components/PayloadClient';
 
 export default async function Home({ searchParams }: { searchParams: Record<string, string> }) {
     const client = new PayloadClient(searchParams);
+
     const [homeData, projectsData, jobsData] = await Promise.all([
         client.fetchHome().catch(() => notFound()),
         client.fetchProjects().catch(() => notFound()),

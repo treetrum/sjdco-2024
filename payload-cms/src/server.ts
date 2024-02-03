ab05dd525199eb144d2afd1b64d339e9667f1564
Sam Davis <sam@sjd.co>
2024-02-03 12:35:11 +1100

    Remove wake-up-cms route and vercel.json
    This commit removes the wake-up-cms route and vercel.json file. It also adds a cron job to keep the server alive every 10 minutes.

--- payload-cms/src/server.ts
+++ payload-cms/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import payload from 'payload';
 import dotenv from 'dotenv';
+import cron from 'node-cron';
 
 dotenv.config();
 const port = process.env.PORT || 3000;
@@ -26,6 +27,12 @@ const start = async () => {
     app.get('/health', (_, res) => {
         console.log('Healthcheck hit!');
         res.send('ok');
+    });
+
+    // Cron job every 10 mins to keep the server alive
+    cron.schedule('*/10 * * * *', () => {
+        console.log('Hitting healthcheck from cronjob');
+        fetch(`https://sjdco-2024-apac.onrender.com/health`);
     });
 
     app.listen(port);

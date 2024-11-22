318546076170699e518e46d985288d3e58f7f6b3
Sam Davis <sam@sjd.co>
2024-11-22 21:14:12 +1100

    Updates

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -3,13 +3,18 @@ import { headers } from 'next/headers';
 import { getPayload } from 'payload';
 import { HomePage } from './HomePage';
 
-export default async function Home({ searchParams }: { searchParams: Record<string, unknown> }) {
+export default async function Home({
+    searchParams,
+}: {
+    searchParams: Promise<{ [key: string]: string | string[] }>;
+}) {
     const payload = await getPayload({ config });
     const auth = await payload.auth({ headers: await headers() });
+    const params = await searchParams;
 
     const homeData = await payload.findGlobal({
         slug: 'home',
-        draft: !!(auth.user && searchParams.preview),
+        draft: !!(auth.user && params.preview),
     });
     const projects = homeData.projects?.filter((p) => typeof p !== 'string') ?? [];
     const jobs = homeData.jobs?.filter((j) => typeof j !== 'string') ?? [];

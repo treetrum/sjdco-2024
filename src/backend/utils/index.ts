74623dbbb1b10b8528c7d2b6359c637737a14b72
Sam Davis <sam@sjd.co>
2024-11-23 11:13:12 +1100

    Fix preview urls

--- src/backend/utils/index.ts
+++ src/backend/utils/index.ts
@@ -1,5 +1,8 @@
-export const generatePreviewUrl = (path: string) => {
-    const url = new URL('/api/preview', process.env.PAYLOAD_PUBLIC_SITE_URL);
+import { getDeployedUrl } from '@/utils/lexical/urls';
+
+export const generatePreviewUrl = async (path: string) => {
+    const base = await getDeployedUrl();
+    const url = new URL('/api/preview', base);
     url.searchParams.set('token', process.env.PAYLOAD_PUBLIC_PREVIEW_TOKEN ?? '');
     url.searchParams.set('path', path);
     return url.toString();
